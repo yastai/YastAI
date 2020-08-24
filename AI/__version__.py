3142b43e570e19f8f1cd27f6b0eefc6d86cffc97
Sebastian Borquez <sebastian.borquez.g@gmail.com>
2020-08-24 02:03:55 -0400

    update version

--- AI/__version__.py
+++ AI/__version__.py
@@ -7,7 +7,7 @@ Information about the current version of the package.
 
 __title__ = 'AI'
 __description__ = 'Simple Image Classificator Models'
-__version__ = '0.0.1'
+__version__ = '0.0.1-alpha.3'
 __author__ = 'Yast.AI'
 __author_email__ = 'yastai.data@gmail.com'
 __license__ = 'MIT'
