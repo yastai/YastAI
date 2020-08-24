b57e3216dd6320fb8458512a01442413b3f18c5f
Sebastian Borquez <sebastian.borquez.g@gmail.com>
2020-08-24 01:32:58 -0400

    add: documentation and imports in data module

--- AI/data/__init__.py
+++ AI/data/__init__.py
@@ -5,3 +5,9 @@ Data
 Load, preprocess and filted data.
 
 """
+
+from constants import *
+from dataset import *
+from generator import *
+from preprocessing import *
+from utils import *
--- AI/visualization/inspect.py
+++ AI/visualization/inspect.py
@@ -6,3 +6,12 @@ Generate plot inspect the blackbox.
 
 Here you can find weights distributios, activations plots and model plot
 """
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import seaborn as sns
+
+
+__all__ = [
+
+]
